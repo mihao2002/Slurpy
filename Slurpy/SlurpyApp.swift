8dd299f0ac590164f86dfb9412dbb88c1d164d9c
Hao Mi <mihao_2002@hotmail.com>
2025-09-24 10:03:55 -0700

    Initial Commit

---
+++ Slurpy/SlurpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlurpyApp.swift
+//  Slurpy
+//
+//  Created by Hao Mi on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SlurpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
